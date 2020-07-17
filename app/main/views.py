e4655b103dc97439fe87495f5cd3b6c2855cb7bb
Ashley Mosoti <ninahb746@gmail.com>
2020-07-17 18:15:22 +0300

    Add a profile template to display user's username,bio and profile picture

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required, current_user
 from ..models import Pitch,User,Comment,Upvote,Downvote
--- app/models.py
+++ app/models.py
@@ -15,12 +15,12 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
     pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
     upvotes = db.relationship('Upvote', backref='user', lazy='dynamic')
     downvotes = db.relationship('Downvote', backref='user', lazy='dynamic')
-    password_hash = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
 
     @property
     def password(self):
