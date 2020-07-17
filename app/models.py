ffeff805786ac8a4c2a32968a328882aa7130b0e
Ashley Mosoti <ninahb746@gmail.com>
2020-07-17 17:18:34 +0300

    Create a registration form for registering new users

--- app/models.py
+++ app/models.py
@@ -1,9 +1,16 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
@@ -13,6 +20,7 @@ class User(db.Model):
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
     upvotes = db.relationship('Upvote', backref='user', lazy='dynamic')
     downvotes = db.relationship('Downvote', backref='user', lazy='dynamic')
+    password_hash = db.Column(db.String(255))
 
     @property
     def password(self):
